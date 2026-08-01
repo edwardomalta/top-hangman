7031be779c6bfc2bcc810440dab8dafdff6e2549
Cesar Quintana <c.evaristo.quintana@gmail.com>
2026-08-01 16:39:46 -0600

    basic mechanism

--- lib/game.rb
+++ lib/game.rb
@@ -11,11 +11,30 @@
 # Then if the letter reveals the last hidden char the player wins and the game ends.
 
 class Game
+  MAX_CHANCES = 10 
+  def initialize
+    @chances_remaining = MAX_CHANCES
+  end
+
   def start
     greet
+    loop_guess
   end
 
   def greet(name: "player")
     puts "Wellcome to a new Hagman Game, #{name}"
+    puts "The computer will select a word from an English dict."
+    puts "the purpose is you have to guess what the word is."
+    puts "for doing so you can write: a character or a word" 
+    puts "(if you think you know what word is it)"
+    puts "You have #{MAX_CHANCES} chances to guess it. Good Luck!"
+  end
+
+  def loop_guess
+    until @chances_remaining < 1 do
+      print "( _ _ _ ) chances remaining #{@chances_remaining} > "
+      answer = gets
+      @chances_remaining -= 1
+    end
   end
 end
